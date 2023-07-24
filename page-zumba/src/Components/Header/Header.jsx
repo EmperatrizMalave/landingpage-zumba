3383cdc47da8d7ae5bce533510f0a59eb94c5a10
emperatrizm <emperatriz.668@gmail.com>
2023-07-24 15:48:17 -0600

    header first changes

---
+++ page-zumba/src/Components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import './styles.css';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <main>
+      <nav className="nav-menu">
+        <ul>
+          <li>Inicio</li>
+          <li>Acerca de</li>
+          <li>Contacto</li>
+        </ul>
+      </nav>
+      </main>
+    </header>
+  );
+};
+
+export default Header;
